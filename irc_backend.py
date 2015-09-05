e8c52f03d65545fbae906516e014dba503a5603f
Niki <dobniki@gmail.com>
2015-09-05 20:33:21 +0300

    Added retrying join if channel is temporily unavailable.

--- irc_backend.py
+++ irc_backend.py
@@ -23,14 +23,18 @@ class IrcBackend(Backend):
         self.reactor.add_global_handler("welcome", self.on_connect)
         self.reactor.add_global_handler("disconnect", self.on_disconnect)
         self.reactor.add_global_handler("pubmsg", self.on_newmsg)
+        self.reactor.add_global_handler("unavailresource", self.on_resource_unavailable)
 
         while 1:
             if self.incoming.poll():
                 info = self.incoming.recv()
-                self.send_msg(info["to"], info["msg"].pretty_print())
+                self._incoming_msg(info["to"], info["msg"].pretty_print())
             
             self.reactor.process_once()
             time.sleep(0.2)
+
+    def join(self, channel):
+        self.channels.append(channel)
 
     def _connect(self):
         logger.debug("IrcBackend: connecting to " + self.name)
@@ -42,13 +46,15 @@ class IrcBackend(Backend):
             time.sleep(5)
             self._connect()
 
+    def _join_channel(self, connection, channel):
+        if irc.client.is_channel(channel):
+            logger.debug("IrcBackend: joining to " + channel + "@" + self.name)
+            connection.join(channel)
+
     def on_connect(self, connection, event):
         logger.debug("IrcBackend: connected to " + self.name)
-
-        for chan in self.channels:
-            if irc.client.is_channel(chan):
-                logger.debug("IrcBackend: joining to " + chan + "@" + self.name)
-                connection.join(chan)
+        for channel in self.channels:
+            self._join_channel(connection, channel)
 
     def on_disconnect(self, connection, event):
         logger.debug("IrcBackend: disconnected from " + self.name + " and trying to reconnect")
@@ -57,15 +63,19 @@ class IrcBackend(Backend):
 
     def on_newmsg(self, channel, event):
         logger.debug("IrcBackend: new message on " + str(event.target) + " -> " + event.arguments[0])
-
         msg = Message(backend=self.name, 
             channel=event.target,
             nickname=event.source.split("!")[0],
             text=event.arguments[0])
+        self._outgoing_msg(msg)
+
+    def on_resource_unavailable(self, connection, event):
+        logger.debug("IrcBackend: resource unavailable")
+        if len(event.arguments) > 0 and irc.client.is_channel(event.arguments[0]):
+            self._join_channel(connection, event.arguments[0])
+
+    def _outgoing_msg(self, msg):
         self.outgoing.put(msg)
 
-    def join(self, channel):
-        self.channels.append(channel)
-
-    def send_msg(self, channel, message):
+    def _incoming_msg(self, channel, message):
         self.server.privmsg(channel, unicode(message))
