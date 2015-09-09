b94eb5a5a69429b6e0d15c3944b69cb57989446e
ndob <dobniki@gmail.com>
2015-09-09 21:59:14 +0300

    Implemented media message handling for whatsapp.

--- msgcenter/whatsapp_backend.py
+++ msgcenter/whatsapp_backend.py
@@ -12,21 +12,50 @@ from yowsup.stacks import YowStackBuilder
 from threading import Thread
 import time
 
+def message_to_text(entity):
+    ret = ""
+    if entity.getType() == "text":
+        ret = entity.getBody()
+    elif entity.getType() == "media":
+        if entity.getMediaType() == "image":
+            ret = "*IMAGE* " + entity.getMediaUrl()
+        elif entity.getMediaType() == "video":
+            ret = "*VIDEO* " + entity.getMediaUrl()
+        elif entity.getMediaType() == "audio":
+            ret = "*AUDIO* " + entity.getMediaUrl()
+        elif entity.getMediaType() == "location":
+            ret = "*LOCATION* http://www.google.com/maps/place/" + entity.getLatitude() + "," + entity.getLongitude()
+        elif entity.getMediaType() == "vcard":
+            ret = "*VCARD* " + entity.getName()
+        else:
+            ret = "*UNKNOWN MEDIA*"
+            logger.warning("WhatsAppBackend: formatting for %s not implemented. Entity: %s" % (entity.getType(), entity))
+    else:
+        ret = "*UNKNOWN MESSAGE*"
+        logger.warning("WhatsAppBackend: formatting for %s not implemented. Entity: %s" % (entity.getType(), entity))
+
+    if hasattr(entity, "getCaption") and callable(getattr(entity, "getCaption")):
+        ret += " "
+        ret += entity.getCaption()
+
+    return ret
+
 class MsgCenterLayer(YowInterfaceLayer):
     PROP_OUTGOING = "com.github.ndob.msgcenter.whatsappbackend.incoming"
     EVENT_NEW_MSG = "com.github.ndob.msgcenter.whatsappbackend.new_message"
 
     def onEvent(self, layerEvent):
         if layerEvent.getName() == self.__class__.EVENT_NEW_MSG:
-            self.incoming_msg(layerEvent.getArg("to"), layerEvent.getArg("text"))
+            self._incoming_msg(layerEvent.getArg("to"), layerEvent.getArg("text"))
             return True
 
     @ProtocolEntityCallback("message")
     def on_message(self, messageProtocolEntity):
-        if messageProtocolEntity.getType() == "text":
-            self.on_text_message(messageProtocolEntity)
-        else:
-            logger.warning("WhatsAppBackend: other than text-messages not implemented")
+        msg = Message(backend="whatsapp1",
+            channel = messageProtocolEntity.getFrom(),
+            nickname = messageProtocolEntity.getNotify(),
+            text = message_to_text(messageProtocolEntity))
+        self.getProp(self.__class__.PROP_OUTGOING).put(msg)
 
         # Acks, that message has "arrived"
         self.toLower(messageProtocolEntity.ack())
@@ -37,14 +66,7 @@ class MsgCenterLayer(YowInterfaceLayer):
     def on_receipt(self, entity):
         self.toLower(entity.ack())
 
-    def on_text_message(self,messageProtocolEntity):
-        msg = Message(backend="whatsapp1",
-            channel=messageProtocolEntity.getFrom(),
-            nickname=messageProtocolEntity.getNotify(),
-            text=messageProtocolEntity.getBody())
-        self.getProp(self.__class__.PROP_OUTGOING).put(msg)
-
-    def incoming_msg(self, channel, message):
+    def _incoming_msg(self, channel, message):
         outgoingMessage = TextMessageProtocolEntity(message, to = channel)
         self.toLower(outgoingMessage)
 
